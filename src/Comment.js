e4eb215c608865ae55144c027b82d43ba098bf0b
Randi Schreiner <mimirandi21@gmail.com>
2021-02-03 18:00:26 -0600

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,9 @@
 //your code here
+import React, { Component } from "react";
+
+export default class BlogPost extends Component {
+	state = { commentText: [] };
+	render() {
+		return <div className="comment">{this.props.commentText}</div>;
+	}
+}
